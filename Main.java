eef646530f7895ded12a7b0f579444195fd6c7cd
Anurag kumar <kumaranurag9795@gmail.com>
2026-08-14 19:59:41 +0530

    adding some parent and child class encapsulation concept

---
+++ Main.java
@@ -0,0 +1,18 @@
+class Parent {
+    static void show() {
+        System.out.println("Parent");
+    }
+}
+
+class Child extends Parent {
+    static void show() {
+        System.out.println("Child");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Parent obj = new Child();
+        obj.show();
+    }
+}
